56712d9cdab7fe7263565f63f5d76b39a5b8cbda
Alexiooooo <83837616+Alexiooooo@users.noreply.github.com>
2022-03-27 01:25:20 +0200

    Update table2.cpp

--- table2.cpp
+++ table2.cpp
@@ -5,11 +5,12 @@ int m,n,chislo[1001][1001],suma[1001][1001],naiM,posledno[1001];
 const int INF=100000001;
 bool ready[1001][1001];
 int sum(int x,int y){
-    if(ready[x][y]){
-        return suma[x][y];
-    }
-    else if(x>=m||y>=n||x<0||y<0){
+    
+    if(x>=m||y>=n||x<0||y<0){
         return INF;
+    }
+    else if(ready[x][y]){
+        return suma[x][y];
     }
      else if(y==0){
         suma[x][0]=chislo[x][0];
@@ -29,13 +30,6 @@ for(int i=0;i<m;i++){
         cin>>chislo[i][j];
     }
 }
-/*
-for(int l=0;l<m;l++){
-    posledno[l]=sum(l,n-1);
-}
-sort(posledno,posledno+m);
-cout<<posledno[0];
-*/
 
 sum(0,n-1);
 naiM=suma[0][n-1];
